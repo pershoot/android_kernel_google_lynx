498f3ab6c54e4140d2309bc214dc2555452d5979
pershoot <190600+pershoot@users.noreply.github.com>
2026-06-16 18:43:48 -0400

    google_module_patches: Update CSF FW for Mali (Android 17)
    -private/google-modules/gpu
    
    -9/9/26: gpu/mali_kbase (csf firmware): release/default FW strings (r54p3.bin)

--- private/google-modules/gpu/borr_mali_kbase/csf/mali_kbase_csf_firmware.c
+++ private/google-modules/gpu/borr_mali_kbase/csf/mali_kbase_csf_firmware.c
@@ -56,8 +56,8 @@
 #include <linux/delay.h>
 #include <linux/version_compat_defs.h>
 
-static char release_fw_name[] = "mali_csffw-r51p0.bin";
-static char default_fw_name[] = "mali_csffw.bin";
+static char release_fw_name[] = "mali_csffw-r54p3.bin";
+static char default_fw_name[] = "mali_csffw-r54p3.bin";
 module_param_string(fw_name, release_fw_name, sizeof(release_fw_name), 0644);
 MODULE_PARM_DESC(fw_name, "firmware image");
 
